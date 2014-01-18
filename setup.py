ac330fd04163a9026c0de8d8f6ad615f0c4caa19
emilssolmanis <emils.solmanis@gmail.com>
2014-01-18 22:39:47 +0200

    forgot the damn MANIFEST file & install failed without README

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
+import os
 
+setup_pth = os.path.dirname(__file__)
+readme_pth = os.path.join(setup_pth, 'README.md')
 
 setup(
     name='hurr-durr',
-    version = '0.0.1',
+    version='0.0.3',
     description='Streaming async interface to 4chan boards',
     author='Emils Solmanis',
     author_email='emils.solmanis@gmail.com',
     license='Apache License (2.0)',
     keywords='4chan stream async',
     url='https://github.com/emilssolmanis/hurr-durr',
-    download_url='https://github.com/emilssolmanis/hurr-durr/archive/0.0.1.tar.gz',
-    long_description=open('README.md').read(),
+    download_url='https://github.com/emilssolmanis/hurr-durr/archive/0.0.3.tar.gz',
+    long_description=open(readme_pth).read(),
 
     packages=find_packages(),
     scripts=['hurr-durr'],
